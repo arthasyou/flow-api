e8d236e1b5baeb0a7e080c578990ddcf2b0ebded
Ancient <arthas.you@163.com>
2025-06-04 17:59:00 +0800

    feat: adding logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod database;
 mod error;
 mod handlers;
+mod logging;
 mod models;
 mod routes;
 mod settings;
@@ -9,11 +10,15 @@ use std::sync::Arc;
 
 use service_utils_rs::services::{db::init_db, http::http_server, jwt::Jwt};
 use settings::Settings;
+use tracing::{error, info};
 
-use crate::database::create_tables;
+use crate::{database::create_tables, logging::init_tracing_to_file};
 
 #[tokio::main]
 async fn main() {
+    init_tracing_to_file();
+    info!("服务已启动");
+    error!("错误日志示例");
     let settings = Settings::load("config/services.toml").unwrap();
     init_db(settings.surrealdb).await.unwrap();
     create_tables().await.unwrap();
