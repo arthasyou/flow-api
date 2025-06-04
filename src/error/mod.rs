e77eb8d2ff7c129e4dda797162004543fb88d869
Ancient <arthas.you@163.com>
2025-06-04 18:39:56 +0800

    Update mod.rs

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -15,16 +15,6 @@ pub enum Error {
 
     #[error("serde error: {0}")]
     SerdeError(#[from] serde_json::Error),
-
-    #[error("{message:} ({line:}, {column})")]
-    CustomError {
-        message: String,
-        line: u32,
-        column: u32,
-    },
-
-    #[error("error message: {0}")]
-    ErrorMessage(String),
 }
 
 pub type Result<T, E = Error> = core::result::Result<T, E>;
