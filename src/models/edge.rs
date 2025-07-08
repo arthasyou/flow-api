ab181afd63b04a94edb028a2a48815774ccc8f5a
Ancient <arthas.you@163.com>
2025-07-08 09:12:10 +0800

    feat: redesign edge

--- src/models/edge.rs
+++ src/models/edge.rs
@@ -2,11 +2,25 @@ use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
 #[derive(Debug, Serialize, Deserialize, ToSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct MarkerEnd {
+    pub arrow_type: String,
+    pub color: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+#[serde(rename_all = "camelCase")]
 pub struct Edge {
     pub id: String,
     pub source: String,
     pub target: String,
-    pub handle: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub source_handle: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub target_handle: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub marker_end: Option<MarkerEnd>,
+    pub animated: bool,
 }
 
 impl From<Edge> for workflow_rs::model::graph_data::EdgeData {
